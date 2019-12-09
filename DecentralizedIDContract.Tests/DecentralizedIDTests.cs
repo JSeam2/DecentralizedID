7a03b01d3c1df6eeeb1f15083685944603cec9e7
jseam2 <joshia_seam@mymail.sutd.edu.sg>
2019-12-10 00:12:07 +0800

    Initial Commit

---
+++ DecentralizedIDContract.Tests/DecentralizedIDTests.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using Stratis.SmartContracts;
+using Stratis.SmartContracts.CLR;
+using System;
+using System.Reflection;
+using Xunit;
+
+// TODO
+namespace DecentralizedIDContract.Tests
+{
+    public class DecentralizedIDTests
+    {
+        private readonly Mock<IContractLogger> mockContractLogger;
+        private readonly Mock<ISmartContractState> mockContractState;
+        private readonly Mock<IPersistentState> mockPersistentState;
+        private readonly Mock<IInternalTransactionExecutor> mockInternalExecutor;
+
+        public DecentralizedIDTests()
+        {
+            this.mockContractLogger = new Mock<IContractLogger>();
+            this.mockContractState = new Mock<ISmartContractState>();
+            this.mockPersistentState = new Mock<IPersistentState>();
+            this.mockInternalExecutor = new Mock<IInternalTransactionExecutor>();
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
